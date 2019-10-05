30c9e1c9a772a9f24424ed50c8d128285af8b953
Denali Balser <denali.balser167@gmail.com>
2019-10-05 15:34:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,12 +12,12 @@ class App < Sinatra::Base
     @num.to_s
   end
 
-  get get '/say/:number/:phrase' do
+  get '/say/:number/:phrase' do
+    str = ""
     @num = params[:number].to_i
     @phrase = params[:phrase]
-    @num.times do
-      puts "#{@phrase}"
-    end
+    @num.times { str += "#{@phrase}\n" }
+    str
   end
 
 end
